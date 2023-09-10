5d05a0fd1fbb61487debf949d47fe92b3501a58c
opama2 <95030993+opama2@users.noreply.github.com>
2023-09-10 13:25:24 +0000

    new file:   index.css 	new file:   index.html 	new file:   index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', function (e) {
+        
+        if (e.target.innerText == 'C') {
+            display.innerText = '';
+        }
+        else if (e.target.innerText == '=') {
+            try{
+                display.innerText = eval(display.innerText);
+            } catch {
+                display.innerText = "Error"
+            };
+        } else if (e.target.innerText == '←') {
+            display.innerText = display.innerText.slice(0,-2);
+        }
+        else {
+            display.innerText += e.target.innerText
+        }
+    });
+});
